36207e40cc0d5ab815b7adbe1d07da5c90a04d1f
Sonya Rahayu <32585915+sonyarah@users.noreply.github.com>
2017-12-16 16:50:11 +0700

    Latihan Overriding Inheriten

---
+++ Latihan Overriding Inheriten/Main.java
@@ -0,0 +1,18 @@
+public class Hewan{
+	String makan;
+
+	public Hewan(String makan){
+	this.makan = makan;
+	}
+}
+
+	class Ayam extends Hewan {
+
+	public Ayam (String makan){
+	super (makan);
+	System.out.println(makan);
+		}
+	}
+	public static void main (String[] args){
+		Hewan hwn = new Hewan("makan");
+}
